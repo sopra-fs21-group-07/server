7d336bcd7e879ee464e6ece5d9dfbd0f485096d3
sumsumcity <raffi_waespi@hotmail.com>
2021-03-25 09:34:31 +0100

    email unique

--- src/main/java/sopra/appuser/AppUser.java
+++ src/main/java/sopra/appuser/AppUser.java
@@ -23,10 +23,16 @@ public class AppUser implements UserDetails {
     @GeneratedValue
     private Long id;
 
+    @Column
     private String firstName;
+
+    @Column
     private String lastName;
-    @Column(nullable = false)
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column
     private String password;
     @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
