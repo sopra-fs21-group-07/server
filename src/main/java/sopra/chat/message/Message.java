17910dacb5153ed1d210ba15c08d519c1d9f62d1
sumsumcity <raffi_waespi@hotmail.com>
2021-04-24 15:24:11 +0200

    chat_function bug fix

---
+++ src/main/java/sopra/chat/message/Message.java
@@ -0,0 +1,39 @@
+package sopra.chat.message;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import sopra.chat.message.MessageType;
+import sopra.chat.user.User;
+
+import java.io.Serializable;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Message implements Serializable{
+
+    private User user;
+    private MessageType type;
+    private String data;
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getData(){
+        return data;
+    }
+}
---
+++ src/main/java/sopra/chat/message/MessageType.java
@@ -0,0 +1,5 @@
+package sopra.chat.message;
+
+public enum  MessageType {
+    USER_JOINED, TEXT_MESSAGE, USER_LEFT, USER_JOINED_ACK
+}
